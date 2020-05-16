d8bf2f2f236aa03028c5a40f68c26765950a9a25
Francisco Morais <fgmorais18@gmail.com>
2020-05-15 22:11:57 -0300

    add proteção matrícula

--- lista6/exe02.c
+++ lista6/exe02.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define quantaluno 15
-#define quantquest 10
+#define quantaluno 15 
+#define quantquest 10 
+#define mediaaprov 6 //média de aprovação
 
 int main(){
 
 	char gabarito[quantaluno - 1], resposta[quantaluno - 1];
 	//são 15 alunos, mas array vai de [0 a 14], logo precisa subtrair 1 para dar certo.
+
+	int matricula[quantaluno - 1];
 
 	float aprov_perc;
 	float aprovados = 0;
@@ -17,18 +20,33 @@ int main(){
 
 	printf("Digite a resposta da \n");
 	for(int i = 0; i < quantquest; i++) {
-		printf("Questao %d: ", i + 1);
-		scanf("%s", &gabarito[i]);
+		printf("Questão %d: ", i + 1);
+		scanf(" %c", &gabarito[i]);
 	}
 
 	for(int i = 0; i < quantaluno; i++){
-		
+
 		printf("\nDigite o número do aluno: ");
 		scanf("%d", &numero);
 
+		//checar se a matricula foi previamente registrada		
+		for(int v = 0; v < quantaluno; v++){
+
+			while(numero == matricula[v]) {
+				printf("Número de matrícula previamente registrado! Digite outro número: ");
+				v = 0;
+				scanf("%d", &numero);
+			}
+
+		}
+
+		matricula[i] = numero;
+
 		for(int j = 0; j < quantquest; j++){
-			printf("Respota questão %d: ", j+1);
-			scanf("%s", &resposta[j]);
+			
+			printf("Resposta questão %d: ", j+1);
+			scanf(" %c", &resposta[j]);
+
 			if(resposta[j] == gabarito[j]){
 				pontos++;
 			}
@@ -36,7 +54,7 @@ int main(){
 
 		printf("A nota do aluno %d foi %d pontos\n\n", numero, pontos);		
 
-		if(pontos >= 6){
+		if(pontos >= mediaaprov){
 			aprovados = aprovados + 1;	
 		}
 
