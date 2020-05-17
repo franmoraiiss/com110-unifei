b13f96554f060d3e4642a7905d5080048b5702c3
Francisco Morais <fgmorais18@gmail.com>
2020-05-17 17:38:33 -0300

    add proteção

--- lista6/exe05.c
+++ lista6/exe05.c
@@ -10,24 +10,37 @@ int main(){
 	int n;
 
 	printf("- Criptografia de Cesar -\n");
+
+	start:
 	printf("Escreva a palavra a ser criptografada: ");
 	scanf("%s", palavra);
 
 	printf("Digite a quantidade n de letras a serem puladas: ");
 	scanf("%d", &n);
 
+	//Número negativos são usados para descriptografar a cifra de Cesar, por isso não é permitido
+	//Caso ele digite 0 não vai criptografar nada, então não faz sentido aceitar 0.
+	//Não faz sentido digitar número >= 26, pois digitar 26 é o mesmo que digitar 0, a frase não muda.
+	//Por exemplo: 'a' + 26 = 'a', pois ao passar da letra 'z', retornamos a letra 'a'.
+
+	while(n <= 0 || n >= 26) {
+		printf("=> Valor inválido. Digite um número entre 0 e 26: ");
+		scanf("%d", &n);
+	}
+
 	int i = 0;
 	while(palavra[i] != '\0') {
 		palavrafinal[i] = palavra[i] + n;
 
+		//Ao digitar ponto, virgula, chaves, ou qualquer caracter de decimal < 97 e decimal > 122, o programa não aceita
+		if((palavra[i]) < 97 || (palavra[i]) > 122) {
+			printf("\n=> Possui caracter inválido! Digite apenas letras.\n");
+			goto start;
+		}
+
 		//Caso passar do caracter 'z' (dec = 122), irá voltar ao caracter 'a' (dec = 97)
 		if((palavra[i] + n) > 122) {
 			palavrafinal[i] = palavrafinal[i] - 26;
-		}
-
-		//Caso o caracter seja anterior ao 'a' (dec < 97)
-		if((palavra[i] + n) < 97){
-			palavrafinal[i] = palavrafinal[i] + 26;
 		}
 
 		i++;
